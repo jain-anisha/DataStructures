ebfd41498cda7d2fc0dd1a7fc6642c88883aa528
Anisha Jain <helloanishaj@gmail.com>
2022-10-20 16:37:17 +0530

    First commit.

---
+++ Sorting/SelectionSort.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void swap(int* x, int* y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main(int i, int array[x])
+{
+    /* code */
+
+    return 0;
+}
