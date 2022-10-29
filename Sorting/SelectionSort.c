377937a494a1c9caefd81e79eef66e6c45946b2d
Anisha Jain <helloanishaj@gmail.com>
2022-10-29 18:54:47 +0530

    ss

--- Sorting/SelectionSort.c
+++ Sorting/SelectionSort.c
@@ -11,6 +11,14 @@ void swap(int* x, int* y)
 int main(int i, int array[x])
 {
     /* code */
+    // 
+    printf("Enter number of elements in your list:");
+    scanf("%d", &n);
+    printf("Enter %d Numbersn", n);
+    for (i = 0; i < n - 1; i++) {
+        for(i = 0; i < n - 1; i++){
+            pos = i;
 
-    return 0;
+        }
 }
+    return 0;
