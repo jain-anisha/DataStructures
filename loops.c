79186fa118e0dc23098343853fd5f6110be58fd4
Anisha Jain <helloanishaj@gmail.com>
2022-10-30 22:09:24 +0530

    Added factorial func

--- loops.c
+++ loops.c
@@ -3,7 +3,7 @@
 
 // prints all natural numbers before n
 
-int main() {
+/*int main() {
     int n = 0;
     printf("Input ending number:");
     scanf("%d", &n);
@@ -15,5 +15,24 @@ int main() {
 
     return 0;
 }
+*/
 
-// 
+//Now writing a loop that gives the factorial
+
+int main() {
+    // need a counter and an input value
+    // input stored in n, counter is i, previous product in p
+    int n;
+    int i = 1;
+    int p = 1;
+    printf("Input integer:");
+    scanf("%d", &n);
+    for (i; i < n; ++i)
+    {
+        p = p * i;
+    }
+    p = p * n;
+    printf("The factorial of %d is %d", n, p);
+    return 0;
+
+}
