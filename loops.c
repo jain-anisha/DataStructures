fefd6162c28af7af400c4aca059a784caa7002e3
Anisha Jain <helloanishaj@gmail.com>
2022-10-30 13:50:33 +0530

    more loops

--- loops.c
+++ loops.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// prints all natural numbers before n
+
 int main() {
-    for(int i, i<n, i++){
-        printf("ff");
+    int n = 0;
+    printf("Input ending number:");
+    scanf("%d", &n);
+    int i;
+    for(i = 1; i<n; ++i)
+    {
+        printf("%d ", i);
     }
 
     return 0;
 }
+
+// 
