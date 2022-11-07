d1f5f9ed9e4d9d5e94c67ac07dff665dcc13e6e2
Anisha Jain <helloanishaj@gmail.com>
2022-11-07 23:25:56 +0530

    1st attempt on sort

--- Sorting/heapSortArray.c
+++ Sorting/heapSortArray.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 void swap(int* x, int* y);
-void heapify(int array[], int n);
+int heapify(int array[], int n, int start);
 void sort(int n, int array[]);
 
 //representing a heap in an array
@@ -15,45 +15,43 @@ void swap(int* x, int* y)
     // swapping places of two different values
 }
 
-void heapify(int array[], int n){
-    int lChild, rChild, p, j, i;
-    // first, need to find the index of the element where we start the heap
-    //lChild = (2*p) + 1;
-    //rChild = (2*p) + 2;
-    //ABOVE WRITTEN FOR NOTES
+int heapify(int array[], int n, int start){
+    int lChild, rChild, greatest;
+    lChild = (2*start) + 1;                     //calculating the left & right child
+    rChild = (2*start) + 2;
+    greatest = start;                           //intializating the greatest element at the start element
 
-    //need to traverse the list
-    /*Intializations: 
-    i = counter
-    lChild, rChild: left and right child
-    need to have it so that i is greater than lChild and rChild
-    EXIT CONDITION: when i = 0, we stop
-    INV: i <= p; i > 0 
-    */
-    p = n/2;                                // starting at the first elements in the second to last row
-    for(i = p; i = 0; i--){
-        j = i;
-        lChild = (2*i) + 1;
+    if(lChild < n && rChild < n){
+        if(array[lChild] > array[greatest]){
+            greatest = lChild;                  // storing the index point of the larger element btwn lChild and the root
         }
-        while(&array[lChild] != NULL ){     //checking if root has children
-                                            //EDIT: this step is not necessary, since all the elements p = n/2...
-                                            //.. has a child out of the definition of p = n/2
-            lChild = (2*j) + 1;
-            rChild = (2*j) + 2;
-            if (&array[j] < &array[rChild]){
-                swap(&array[j], &array[rChild]);
-            }
-            if (&array[j] < &array[lChild]){
-                swap(&array[j], &array[lChild]);
-            //ERROR: I am not going through the heap properly
-            //In this case, i am still going through the heap without necessity, so need to find a way to go through it
-
-            }
+        if(array[rChild] > array[greatest]){
+            greatest = rChild;                  //storing the index point of the larger element btwn the prev largest...
+        }                                       //... element, and rChild
+        //now we have three cases of the largest element: the original, lChild, and rChild. We will need to swap in all...
+        //cases other than if the largest element found is the same as the original root. SO:
+        if (greatest != start){
+            swap(&array[greatest], &array[start]);
+            //Now, we need to heapify the rest of the elements under the root
+            heapify(array, n, greatest);
         }
     }
+}
 
 //int maxEle(int array[], int n){
 void sort(int n, int array[]){
+    //input: max heap
+    //INV: i < n 
+    //EXIT: when i = n
+    int i = 0;
+    for(i; i<n; i++){
+        heapify(array, n, n/2 - 1);
+    }
+
+    for(i = n - 1; i<= n; i++){
+        swap(&array[n], &array[0]);
+        heapify(array, n, n/2);
+    }
 
 }
 
@@ -67,14 +65,12 @@ int main(){
     for (i = 0; i < length; i++){
         scanf("%d", &array[i]);
     }
-    heapify(array, length);
+    sort(length, array);
     printf("The array is: ");                   //printing array
     for(i = 0; i<length; i++) {
         printf("%d ", array[i]);
     }
     return 0;
-
-
 }
 
 // Seg fault...
