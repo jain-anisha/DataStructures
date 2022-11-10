2d16b75a49df8abd1816fd398ff7e529f1cd6b13
Anisha Jain <helloanishaj@gmail.com>
2022-11-11 04:27:47 +0530

    another attempt

--- Sorting/RevLinkedList.c
+++ Sorting/RevLinkedList.c
@@ -3,35 +3,102 @@
 First, making a linked list, based on the code from the linked list
 */
 
-
 //from the prev linked list code
-struct Node
-{   int val; // can only store integer data types
-    struct Node* next;
-    // creates a data type called Node that stores the data of the linkedlist and the point of the next node.
+struct node{
+    int val; 
+    struct node *next;          // creating the node
 };
 
-void revList(Node* head ){
-    if(!head){
-        return;}
-    if 
-    
-    
+void revList(struct node* header){
+    //essentially, when we are given the head node, we switch the head node
+    // we make the head node point to NULL, while having another pointer point to another
+    if(!header){
+        return;}        //if head does not exist, there's no linked list
+    /* We need 3 pointers: one to point at the current node, one to point at prev node, and one to point at next node
+    while the current node is NOT pointing to a NULL val (AKA is not the last val in the list), store the next ptr
+    as the current pointing to the next val, 
+    then, reverse the ptr of the current,
+    then, move the ptrs one step ahead
+
+    INV: prev < current < next
+    exit condition: when current == NULL (aka, we have reached the end of the linked list)
+
+    after switching all the values, we need to deal with the previous head
+    we put the head ptr to the last prev val, and we have finished
+
+    */
+    struct node* prev = NULL ;
+    struct node* next = NULL;
+    struct node *curr = header;
+
+    while(curr != NULL){
+        next = curr -> next;
+        curr -> next = prev;
+
+        prev = curr;
+        curr = next;
+    }
+    header = prev;
 }
 
-void Listprint(struct Node* head){
-    struct Node* i = head;
+struct node * linklist(int n){
+    //taken from create linked list code
+    int val;
+    struct node *temp;                  // points to the 
+    struct node *new;
+    struct node *head = NULL;       //making the head node as a global
+
+
+    head = (struct node *)malloc(sizeof(struct node));      //making the head node
+
+    if(n <= 0 || head == 0){
+        return;
+    }
+
+    //head is the start of the linked list, that holds no value
+
+    //now, writing the first node
+    printf("Enter the values of %d nodes:\n", n);
+    scanf("%d", &val);
+    head -> next = NULL;                // the next pointer points to a null val
+    head -> val = val;                  // the val in the node is the val inputed into function
+    temp = head;                        // special condition for the first node, since we need to connect the head
+
+    //now, need to link the other values
+    // to do this, we will use a for loop
+    // 1 <= i < n : INV
+    for(int i = 1; i < n; i++){
+        new = (struct node *)malloc(sizeof(struct node));
+            // same the the previous print statement, no difference
+            scanf("%d", &val);
+            new -> next = NULL;     // the next ptr of the new node is NULL
+            new -> val = val;       // the value of val in the new node is the inputted val
+
+            temp -> next = new;     //
+            temp = temp -> next;
+    }
+    return head;
+}
+
+void Listprint(struct node* head){
+    struct node* i = head;
     while (i != NULL) {
-        printf("%d ", i->data);
+        printf("%d ", i->val);
         i = i->next;
     }
 }
 
 // linked list with four elements
+
 int main()
 {
+    int num; 
 
-    }
-
-    RevList(a, b, c, d)    
+    printf("Enter number of nodes: ");
+    scanf("%d", &num);
+    struct node *head = linklist(num);
+    // how do we print a list?? no clue if code is correct as of now
+    revList(head);
+    Listprint(head);    
     return 0;
+}
