79d6f107512262ceec4a8e91761a32e578db32d1
Anisha Jain <helloanishaj@gmail.com>
2022-11-14 04:13:23 +0530

    comments

--- Sorting/qsortlinked.c
+++ Sorting/qsortlinked.c
@@ -25,8 +25,8 @@ struct node * linklist(int n){
 
     head = (struct node *)malloc(sizeof(struct node));      //making the head node
 
-    if(n <= 0 || head == 0){
-        return;}
+    if(n <= 1 || head == NULL){
+        return head;}
 
     //head is the start of the linked list, that holds no value
 
@@ -58,45 +58,66 @@ struct node * linklist(int n){
 struct node * findlast(struct node *head){
     //TRAVERSING THE LIST to find the last node
     if (head == NULL){
-        return;             //linked list is not a list
+        return head;             //linked list is not a list
     }
     //need to traverse the list, until the next element has NULL as the next address that it is pointing to 
     struct node *temp = head;       //temp var that will traverse list, starts at first element
-    int i = 0;                      //index: counts num of loops
+   
     while(temp-> next != NULL){
         temp = temp -> next;
         //now, temp must be: temp = temp -> next = NULL
     }
     assert(temp -> next == NULL);
-    struct node *last = temp;
+    end = temp;
     //creating node to store the last element of the list
-    return last;
+    return end;
 }
 
-void partition(struct node *first, struct node* last){
-    // creating a partiton of greater and lesser values
-    // the greater 
-    //the pivot element, aka the last in the list
-    // need a pointer for the greatest wall, and a pointer for the lesser wall, and the 
-    // need a pointer for the current element as well.
-    struct node *pivot, *greater, *less, *curr, *next;
+struct node * partition(struct node *first, struct node* pivot){
+    /* essentially, will part the list into two: greater and lesser
 
-    //last element is made to be the pivot
-    pivot = last;
-    //need a loop to go through entire list
-    int i;
-    for(i = 0; i < )
+    comparing the pivot by the value of the current node
+    two cases:
+        if greater, swap curr-> next with 
+    */
+
+
+    // need a pointer for the greatest wall, and a pointer for the lesser wall, and the current element
+    // less points second to last element
+    // need a pointer for the current element as well.
+    struct node *greater,  *less, *curr, *prevgreat = first;
+    struct node *next;
+
+    /* to find second to last element to point to, will traverse the list 
+    as long as the next element does not equal pivot, we go to the next element
+    */
+    while(greater->next -> next != pivot){
+        greater = greater -> next;
+    }
+
+    prevgreat = greater -> next; 
+    assert(greater -> next -> next = pivot);
+
+    curr = curr -> next;
+
+    //need a loop to go through entire list,
+        //exit condition: when the next element in the list is the 
+    while(curr -> next != greater){
+        if (curr -> val > pivot -> val){
+            swap(curr, greater);
+            greater = 
+        }    
     // exit cond: when curr -> next -> next does not exist?
     
     
 }
 
 void qsort(struct node* start, struct node* stop){
-    if ( start == NULL || start->val > stop->val){
+    if ( start == NULL || stop == NULL){
         return;
     }
 
-    struct node* wall = partition(*start, *stop);
+    struct node* wall = partition(start, stop);
 
     qsort(start, wall - 1);          // less list
     qsort(wall + 1, stop);           // greater list
@@ -115,7 +136,6 @@ void printL(){
     printf("%d\n", temp->val);
     // specifically printing out the last node
 }
-
 }
 
 int main()
