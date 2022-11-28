5d64120a2557038fbeb1dee4c1e263eaed5d2c5c
Anisha Jain <helloanishaj@gmail.com>
2022-11-28 15:13:14 +0530

    commenting algo

--- Trees/AVL.c
+++ Trees/AVL.c
@@ -55,8 +55,53 @@ struct node * newinput(struct node * root, int input){
     else if (input > root -> val){
         root -> right = newinput(root -> right, input);
     }
-    return root;
+    else{
+        return root;
+    }
     //calling itself recursively inorder to find the right place to input the new value
+    int bal = balanceVal(root);
+    /* now, must see if the tree is imbalanced, and balance it
+    five cases for bal's value:
+        - balanced:
+            - bal = 0 or 1 or -1
+            - nothing needs to be done
+        - left left imbalanced
+            - bal > 1
+            - input < node's left child's val
+                - right rotate
+        - left right imbalanced
+            - bal > 1
+            - input > node's left child's val
+                - node -> left: right rotate
+                - right rotate
+        - right right imbalanced
+            - bal < -1 
+            - input > node's right child's val
+                - left rotate
+        - right left imbalanced
+            - bal < -1
+            - input < node's right child's val
+                - node -> right: left rotate
+                - left rotate
+    */
+   return root;
+}
+
+int balanceVal(struct node * n){
+    /*
+    in order to balance a tree, need to know the weights of both sides of the tree
+
+    when the node is NULL, the tree must also be empty, so both sides of the tree is the same, which makes
+    ... the balance zero
+
+    when it is not NULL, we find the balance by substracting the right height val by the left height val
+    */
+   if (n == NULL){
+        return 0;
+   }
+   else{
+        return height(n -> left) - height(n -> right);
+   }
 }
 
 void main(){
