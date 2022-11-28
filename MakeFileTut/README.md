4f3418594d25230862f9d5973571cc8af3da4050
Anisha Jain <helloanishaj@gmail.com>
2022-11-28 12:21:09 +0530

    intializing

---
+++ MakeFileTut/README.md
@@ -0,0 +1,4 @@
+# Make File
+
+following the tutorial on https://www.cs.colby.edu/maxwell/courses/tutorials/maketutor/
+
---
+++ MakeFileTut/hellomake.c
@@ -0,0 +1,10 @@
+#include <hellomake.h>
+
+int main() {
+  // call a function in another file
+  myPrintHelloMake();
+
+  return(0);
+}
+
+//copied from https://www.cs.colby.edu/maxwell/courses/tutorials/maketutor/
